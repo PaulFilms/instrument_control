f42a888fe0548e880698dde7da49ae61ef66cda6
PaulFilms <pablopila.spain@gmail.com>
2023-12-18 11:46:00 +0100

    Changes to be committed: 	modified:   INSTRUMENTS/PXI_DCPOWER.py

--- INSTRUMENTS/PXI_DCPOWER.py
+++ INSTRUMENTS/PXI_DCPOWER.py
@@ -17,6 +17,9 @@ __author__ = 'PABLO GONZALEZ PILA <pablogonzalezpila@gmail.com>'
 
 ''' SYSTEM LIBRARIES '''
 from typing import List
+from enum import Enum
+
+''' MAIN LIBRARIES '''
 import nidcpower
 
 
@@ -33,15 +36,20 @@ NMB_FUNCTIONS: List[str] = [
     'MEAS_CURR'
 ]
 
+class TYPES(Enum):
+    VOLTAGE = nidcpower.MeasurementTypes.VOLTAGE
+    CURRENT = nidcpower.MeasurementTypes.CURRENT
+
+class OUT_FUNC(Enum):
+    VOLTAGE = nidcpower.OutputFunction.DC_VOLTAGE
+    CURRENT = nidcpower.OutputFunction.DC_CURRENT
+
 class INSTRUMENT:
     '''
     '''
     def __init__(self, resource: str = ""):
         self.session = nidcpower.Session(resource)
         self.session.initiate()
-        
-        self.type_voltage = nidcpower.MeasurementTypes.VOLTAGE
-        self.type_current = nidcpower.MeasurementTypes.CURRENT
     
     def CLOSE(self) -> None:
         '''
@@ -71,6 +79,7 @@ class INSTRUMENT:
         if len(args) > 0:
             if args[0] == True or args[0] == 1 or args[0] == "1" or args[0] == "ON":
                 output = True
+        # self.session.channels[0].output_enabled = True
         self.session.output_enabled = output
     
     def SET_VOLT(self, *args) -> None:
@@ -78,8 +87,10 @@ class INSTRUMENT:
         arg0: float = Value (Voltage)
         arg1: bool = Channel
         '''
-        channel: int = int(args[1])
         value: float = float(args[0])
+        channel: int = int(args[1])
+        self.session.channels[channel].output_function = OUT_FUNC.VOLTAGE
+        self.session.channels[channel].current_limit = 0.1
         self.session.channels[channel].voltage_level = value
        
     def MEAS_VOLT(self, *args) -> float:
@@ -87,7 +98,7 @@ class INSTRUMENT:
         arg0: bool = Channel
         '''
         channel: int = int(args[0])
-        meas = self.session.channels[channel].measure(self.type_voltage)
+        meas = self.session.channels[channel].measure(TYPES.VOLTAGE)
         return meas
 
     def SET_CURR(self, *args) -> None:
@@ -96,8 +107,10 @@ class INSTRUMENT:
         arg1: bool = Channel
         
         '''
-        channel: int = int(args[1])
         value: float = float(args[0])
+        channel: int = int(args[1])
+        self.session.channels[channel].output_function = OUT_FUNC.CURRENT
+        self.session.channels[channel].voltage_limit = 1.0
         self.session.channels[channel].current_level = value
 
     def MEAS_CURR(self, *args) -> float:
@@ -105,18 +118,18 @@ class INSTRUMENT:
         arg0: bool = Channel
         '''
         channel: int = int(args[0])
-        meas = self.session.channels[channel].measure(self.type_current)
+        meas = self.session.channels[channel].measure(TYPES.CURRENT)
         return meas
 
 ''' TEST
 ----------------------------------------------------------------------- '''
 
-inst = INSTRUMENT("PXI1Slot3")
+# inst = INSTRUMENT("PXI1Slot3")
 
-# idn = inst.DEVICE_INFO()
-# print(idn)
+# # idn = inst.DEVICE_INFO()
+# # print(idn)
 
-inst.SET_VOLT(1, 0)
-inst.SET_VOLT(0, 1)
-inst.SET_VOLT(0, 2)
-inst.OUTPUT(1)
+# inst.SET_VOLT(1, 0)
+# inst.SET_VOLT(0, 1)
+# inst.SET_VOLT(0, 2)
+# inst.OUTPUT(1)
